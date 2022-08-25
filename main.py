c71946c58d8344905cfcc47ae39c34ec7b9ff953
Jörg Rüggberg <joerg.rueggeberg@googlemail.com>
2022-08-25 22:05:39 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+adding = True
+data_mail = []
+data = {
+    "headline": "username,firstname,lastname,email,password",
+    "user": [],
+    "email_start": input("Wie lautet die letzt aktuelle E-Mail? "),
+    "password": input("Welches Passwort? "),
+}
+FILEPATH = "C:/Users/***/Desktop/user.csv"
+umlaute = ["ä", "ö", "ü"]
+umlaute_neu = ["ae", "oe", "ue"]
+
+
+def mail_prep():
+    global data_mail
+    email = data["email_start"]
+    data_mail.append(email.split("@")[0][:-3])
+    data_mail.append(int(email.split("@")[0][-3:]))
+    data_mail.append(email.split("@")[1])
+
+
+def new_user():
+    global data_mail
+    global data
+
+    data_mail[1] += 1
+
+    if len(str(data_mail[1])) == 1:
+        mail_num = f"00{data_mail[1]}"
+    elif len(str(data_mail[1])) == 2:
+        mail_num = f"0{data_mail[1]}"
+    else:
+        mail_num = data_mail[1]
+
+    mail_new = f"{data_mail[0]}{mail_num}@{data_mail[2]}"
+    firstname = input("Vorname? ")
+    lastname = input("Nachname? ")
+
+    # TODO 1: Umlaut-check
+
+    username = f"{firstname[0]}{lastname}".lower()
+    data_user = [f"{username},{firstname.title()},{lastname.title()},{mail_new},{data['password']}"]
+    data["user"].append(data_user)
+
+
+def next_user():
+    global adding
+    adding_input = input("Weiteren Nutzer anlegen (j/n)? ").lower()
+    if adding_input == "n":
+        adding = False
+    elif adding_input == "j":
+        adding = True
+    else:
+        print("Falsche Eingabe. Vorgang abgebrochen.")
+
+
+def write_data():
+    for i in data["user"]:
+        with open(FILEPATH, "a+") as file_user:
+            file_user.write(f"{str(i[0])}\n")
+
+
+# SCRIPT
+with open(FILEPATH, "w") as file:
+    file.write(f"{data['headline']}\n")
+
+mail_prep()
+
+while adding:
+    new_user()
+    next_user()
+
+write_data()
